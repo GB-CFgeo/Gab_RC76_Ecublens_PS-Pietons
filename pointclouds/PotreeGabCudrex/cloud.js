bc33a5db76884c562c15b9176dce23ff54eacd25
Giuseppe Barbiera <82044175+GB-CFgeo@users.noreply.github.com>
2025-10-06 10:36:36 +0200

    Gab_RC76_Ecublens_PS-Pietons
    Gab_RC76_Ecublens_PS-Pietons

---
+++ pointclouds/PotreeGabCudrex/cloud.js
@@ -0,0 +1,67 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 2637632,
+    "boundingBox": {
+        "lx": 2532373.7623012627,
+        "ly": 1153623.4110702768,
+        "lz": 418.8109461798149,
+        "ux": 2532401.0145012626,
+        "uy": 1153650.6632702768,
+        "uz": 446.06314617980299
+    },
+    "tightBoundingBox": {
+        "lx": 2532373.7623012627,
+        "ly": 1153623.4110702768,
+        "lz": 418.8109461798149,
+        "ux": 2532401.0145012626,
+        "uy": 1153635.2825702767,
+        "uz": 426.4618561798149
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 0.23601096868515016,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
